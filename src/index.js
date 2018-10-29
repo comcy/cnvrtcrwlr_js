f9b5d01852047adacc79492500bea791c6461d89
com_cy <christian.silfang@googlemail.com>
2018-10-29 21:41:35 +0100

    added first functional usage of showdown

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
+#!/usr/bin/env node
+
 var showdown  = require('showdown'),
     converter = new showdown.Converter(),
     text      = '# hello, markdown!',
     html      = converter.makeHtml(text);
+
+
+// TODO: added test for converted print out 
+console.log(html);
+
