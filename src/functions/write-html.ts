31e5ad0df05946f4b8d4f1ebac5de076f5920884
com_cy <christian.silfang@googlemail.com>
2018-11-23 22:45:46 +0100

    added pdf files and refactored code

---
+++ src/functions/write-html.ts
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var getDirName = require('path').dirname;
+
+/**
+ * This function writes content to a file under the given path and filename.
+ * @param path, path and filename of the file which should be written
+ * @param contents, content of the file
+ * @param cb 
+ */
+export function writeHtml(path, contents, cb): void {
+    mkdirp(getDirName(path), function (err) {
+        if (err) return cb(err);
+        fs.writeHtml(path, contents, cb, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        });
+    });
+}
